507ab8c9ca3556741f0a916d080501891c0afc56
xxand <xxand@ASUSTUFA15>
2024-10-10 14:05:53 -0600

    Agregar archivos de proyecto.

---
+++ ESFE.Calculadora.UnitTest.AppMSTest/CalculadoraTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESFE.Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE.Calculadora.Tests
+{
+    [TestClass()]
+    public class CalculadoraTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculadora calculadora = new Calculadora();
+
+            double result = calculadora.Add(5, 3);
+
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            Calculadora calculadora = new Calculadora();
+
+            double result = calculadora.Subtract(5, 3);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculadora calculadora = new Calculadora();
+
+            double result = calculadora.Multiply(5, 3);
+
+            Assert.AreEqual(15, result);
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Calculadora calculadora = new Calculadora();
+
+            double result = calculadora.Divide(6,2);
+
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void DivideByZeroTest()
+        {
+            Calculadora calculadora = new Calculadora();
+
+            double result = calculadora.Divide(5, 0);
+        }
+    }
+}
---
+++ TestProject1/CalculadoraTests.cs
@@ -0,0 +1,72 @@
+﻿using ESFE.Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrecrSum()
+        {
+            Calculadora calculadora = new Calculadora();
+            double num1 = 5.0;
+            double num2 = 3.0;
+
+            double result = calculadora.Add(num1, num2);
+
+            Assert.Equal(8.0, result);
+        }
+
+        [Fact]
+        public void Substract_ShouldReturnCorrectDifference()
+        {
+            Calculadora calculadora = new Calculadora();
+            double num1 = 8.0;
+            double num2 = 3.0;
+
+            double result = calculadora.Subtract(num1, num2);
+
+            Assert.Equal(5.0, result);
+        }
+
+        [Fact]
+        public void MultiPly_ShouldReturnCorrectProduct()
+        {
+            Calculadora calculadora = new Calculadora();
+            double num1 = 4.0;
+            double num2 = 2.0;
+
+            double result = calculadora.Multiply(num1, num2);
+
+            Assert.Equal(8.0, result);
+        }
+
+        [Fact]
+        public void Divide_ShouldReturnCorrectQuotient()
+        {
+            Calculadora calculadora = new Calculadora();
+            double num1 = 10.0;
+            double num2 = 2.0;
+
+            double result = calculadora.Divide(num1, num2);
+
+            Assert.Equal(5.0, result);
+        }
+
+        [Fact]
+        public void Divide_ByZero_ShouldThrowException()
+        {
+            Calculadora calculadora = new Calculadora();
+            double num1 = 5.0;
+            double num2 = 0.0;
+
+            ;
+
+            Assert.Throws<ArgumentException>(() => calculadora.Divide(num1,num2));
+        }
+    }
+}
